64d84836120a9a32f729203f90256e315963bc6e
nahan-mea <24MCAG07@meaec.edu.in>
2024-10-14 11:00:49 +0530

    Delete gtr.py

--- gtr.py
+++
@@ -1,9 +0,0 @@
-numbers=input("Enter integers seperated by spaces:").split()
-modified_list=[]
-for x in numbers:
-    if int(x)>100:
-        modified_list.append('Over')
-    else:
-        modified_list.append(x)
-print("Modified List:",modified_list)
-
