e3d6faa1a414ad9766bfca0c2c78bcb6bf023c96
nahan-mea <24MCAG07@meaec.edu.in>
2024-10-14 11:00:36 +0530

    Delete oc.py

--- oc.py
+++
@@ -1,6 +0,0 @@
-names=input("Enter first names seperated by spaces:").split()
-count_a=sum([name.lower().count('a')
-    for name in names])
-print("Occurences of 'a':",count_a)
-
-                               
