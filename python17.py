14bfcf1ceaea187b909b3628bf8184de2bd8a9e2
nahan-mea <24MCAG07@meaec.edu.in>
2024-10-14 16:10:10 +0530

    Update python17.py

--- python17.py
+++ python17.py
@@ -1,4 +1,13 @@
-dictionary = {'a': 3, 'b': 1, 'c': 2}
-ascending = dict(sorted(dictionary.items()))
-descending = dict(sorted(dictionary.items(), reverse=True))
-print(ascending, descending)
+n = int(input("Enter the number of items in the dictionary:"))
+dictionary = {}
+for _ in range(n):
+    key = input("Enter key:")
+    value = int(input("Enter value:"))
+    dictionary[key] = value
+
+
+ascending = dict(sorted(dictionary.items(), key=lambda item: item[1]))
+descending = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+
+print("Ascending:", ascending)
+print("Descending:", descending)
