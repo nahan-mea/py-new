4ea88658e2ceaea9b80eae99ef7888aef338618e
nahan-mea <24MCAG07@meaec.edu.in>
2024-10-14 16:10:55 +0530

    Update python18.py

--- python18.py
+++ python18.py
@@ -1,4 +1,15 @@
-dict1 = {'a': 1, 'b': 2}
-dict2 = {'b': 3, 'c': 4}
-merged_dict = {**dict1, **dict2}
-print(merged_dict)
+dict1={}
+n1=int(input("how many items in dictionary 1?"))
+for i in range(n1):
+    key=input("enter key:")
+    value=input("enter value:")
+    dict1[key]=value
+dict2={}
+n2=int(input("how many items in dictionary 2?"))
+for i in range(n2):
+    key=input("enter key:")
+    value=input("enter value:")
+    dict2[key]=value
+merged_dict={**dict1,**dict2}
+print("merged dictionary:",merged_dict)
+
